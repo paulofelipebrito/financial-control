2c2a875936222f09aaf904f9825526b5f747a2db
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2023-07-14 10:30:14 -0300

    feat: add optionalParseEnumPipe pipe

---
+++ api/src/shared/pipes/OptionalParseEnumPipe.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { ParseEnumPipe, ArgumentMetadata } from '@nestjs/common';
+
+export class OptionalParseEnumPipe<T = any> extends ParseEnumPipe<T> {
+  override transform(value: T, metadata: ArgumentMetadata){
+    if(typeof value === 'undefined'){
+      return undefined;
+    }
+    
+    return super.transform(value, metadata);
+  }
+}
