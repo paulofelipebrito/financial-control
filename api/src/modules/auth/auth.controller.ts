d79bf946dce27e26e05b12ea398cd83970d2b72a
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2023-07-17 21:00:28 -0300

    feat: add auth module

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -2,18 +2,22 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin';
 import { SignupDto } from './dto/signup';
+import { IsPublic } from 'src/shared/decorators/isPublic';
 
 @Controller('auth')
+@IsPublic()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
+  // @IsPublic()
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 
   @Post('signup')
-  create(@Body() signupDto: SignupDto) {
+  // @IsPublic()
+  signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 }
--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { env } from 'src/shared/config/env';
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
       signOptions: { expiresIn: '7d' },
-      secret: 'unsecure_jwt_secret',
+      secret: env.jwtSecret,
     }),
   ],
   controllers: [AuthController],
